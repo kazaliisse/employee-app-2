3f88aefd413a017b181c687259cc173476dd921a
noor <noorhafowbare@gmail.com>
2024-09-25 01:15:58 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import EmpListing from './EmpListing';
-import EmpCreate from './EmpCreate';
-import EmpDetail from './EmpDetail';
-import EmpEdit from './EmpEdit';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmpListing from "./EmpListing";
+import EmpCreate from "./EmpCreate";
+import EmpDetail from "./EmpDetail";
+import EmpEdit from "./EmpEdit";
 
 function App() {
   return (
     <div className="App">
-      <h1>React JS CRUD Opertations</h1>
+      <h1>React JS project of dealing with crud</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<EmpListing />}></Route>
-          <Route path='/employee/create' element={<EmpCreate />}></Route>
+          <Route path="/" element={<EmpListing />}></Route>
+          <Route path="/employee/create" element={<EmpCreate />}></Route>
 
-          <Route path='/employee/detail/:empid' element={<EmpDetail />}></Route>
-          <Route path='/employee/edit/:empid' element={<EmpEdit />}></Route>
+          <Route path="/employee/detail/:empid" element={<EmpDetail />}></Route>
+          <Route path="/employee/edit/:empid" element={<EmpEdit />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
-
 }
 
 export default App;
